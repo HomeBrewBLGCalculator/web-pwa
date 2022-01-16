cecb4c2a8fd672885780802f878ba06f5ff7d0b4
Szymon Nowicki <sznowicki@eskalacja.com>
2022-01-16 18:54:52 +0100

    PWA offline mode

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,9 @@
 import {manifest, version} from '@parcel/service-worker';
 
+const cacheName = version;
 async function install() {
-  const cache = await caches.open(version);
+  console.warn(manifest);
+  const cache = await caches.open(cacheName);
   await cache.addAll(manifest);
 }
 
@@ -10,8 +12,20 @@ addEventListener('install', e => e.waitUntil(install()));
 async function activate() {
   const keys = await caches.keys();
   await Promise.all(
-    keys.map(key => key !== version && caches.delete(key))
+    keys.map(key => key !== cacheName && caches.delete(key))
   );
 }
 
 addEventListener('activate', e => e.waitUntil(activate()));
+
+async function fetchWorker(e) {
+  e.respondWith((async () => {
+    console.warn(e.request);
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    await fetch(e.request);
+  })())
+}
+
+addEventListener('fetch', fetchWorker)
