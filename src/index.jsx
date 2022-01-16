f1aafdd86d8a2b19c2e46390f3b170c094d4415b
Szymon Nowicki <sznowicki@eskalacja.com>
2022-01-16 18:41:06 +0100

    PWA offline mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 
 const App = () => {
 
-    return <p>Hello world</p>;
+  return <p>Hello world</p>;
 };
 
 const main = () => {
   const root = document.getElementById('root');
 
-  render(<App />, root);
+  render(<App/>, root);
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register(
+      new URL('sw.js', import.meta.url),
+      { type: 'module'}
+    );
+  }
 };
 
 main();
